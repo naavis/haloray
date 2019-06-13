8624e9c7b22eb10c61907a99d3eb1795fd7334fa
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-06-13 20:42:30 +0300

    Simplify condition where rays are discarded

--- src/shaders/raytrace.glsl
+++ src/shaders/raytrace.glsl
@@ -513,7 +513,7 @@ void main(void)
     vec2 projected = fr * vec2(aspectRatio * cos(polar.y), sin(polar.y)) / camera.fov;
     vec2 normalizedCoordinates = 0.5 + projected / PI;
 
-    if (any(lessThanEqual(normalizedCoordinates, vec2(0.0, 0.0))) || any(greaterThanEqual(normalizedCoordinates, vec2(1.0, 1.0))))
+    if (any(lessThanEqual(normalizedCoordinates, vec2(0.0))) || any(greaterThanEqual(normalizedCoordinates, vec2(1.0))))
         return;
 
     ivec2 pixelCoordinates = ivec2(resolution.x * normalizedCoordinates.x, resolution.y * normalizedCoordinates.y);
