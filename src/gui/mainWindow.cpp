ae32061de81e14ad6e9b83d49ca4172826d27b48
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-06-25 21:13:35 +0300

    Connect other widgets to simulation

--- src/gui/mainWindow.cpp
+++ src/gui/mainWindow.cpp
@@ -3,6 +3,8 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QSlider>
+#include <QDoubleSpinBox>
+#include <QComboBox>
 #include "openGLWidget.h"
 #include "../simulation/simulationEngine.h"
 #include "ui_mainWindow.h"
@@ -19,5 +21,25 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
         auto light = mEngine->GetLightSource();
         light.altitude = (float)value;
         mEngine->SetLightSource(light);
+    });
+    connect(ui->sunDiameterSpinner, QOverload<double>::of(&QDoubleSpinBox::valueChanged), [=](double value) {
+        auto light = mEngine->GetLightSource();
+        light.diameter = value;
+        mEngine->SetLightSource(light);
+    });
+    connect(ui->caRatioSpinner, QOverload<double>::of(&QDoubleSpinBox::valueChanged), [=](double value) {
+        auto crystals = mEngine->GetCrystalPopulation();
+        crystals.caRatioAverage = value;
+        mEngine->SetCrystalPopulation(crystals);
+    });
+    connect(ui->caRatioStdSpinner, QOverload<double>::of(&QDoubleSpinBox::valueChanged), [=](double value) {
+        auto crystals = mEngine->GetCrystalPopulation();
+        crystals.caRatioStd = value;
+        mEngine->SetCrystalPopulation(crystals);
+    });
+    connect(ui->cameraProjectionComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), [=](int index) {
+        auto camera = mEngine->GetCamera();
+        camera.projection = (HaloSim::Projection)index;
+        mEngine->SetCamera(camera);
     });
 }
