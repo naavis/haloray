1c0ffeeae30dce1f2b1abf7501b47f45e4e56e5c
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-06-29 20:07:14 +0300

    Make slider spin boxes more precise
    QSlider only supports integers. To make the slider more accurate, all
    the values going in and out are multiplied or divided respectively with
    a constant to have more slider resolution.

--- src/gui/sliderSpinBox.cpp
+++ src/gui/sliderSpinBox.cpp
@@ -1,27 +1,32 @@
 #include "sliderSpinBox.h"
 #include <QHBoxLayout>
 
+const double sliderMultiplier = 100.0;
+
 SliderSpinBox::SliderSpinBox(QWidget *parent) : QWidget(parent)
 {
     mSlider = new QSlider(this);
     mSlider->setOrientation(Qt::Orientation::Horizontal);
-    mSlider->setSingleStep(1);
+    mSlider->setSingleStep((int)sliderMultiplier);
+    mSlider->setPageStep((int)(10 * sliderMultiplier));
 
     mSpinBox = new QDoubleSpinBox(this);
     mSpinBox->setSingleStep(0.1);
-
-    connect(mSlider, &QSlider::valueChanged, [=](int value) {
-        mSpinBox->setValue((double)value);
-    });
-    connect(mSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), [=](double value) {
-        mSlider->setValue((int)value);
-    });
-    connect(mSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &SliderSpinBox::valueChanged);
 
     auto layout = new QHBoxLayout();
     layout->addWidget(mSlider);
     layout->addWidget(mSpinBox);
     setLayout(layout);
+
+    setMinimumWidth(400);
+
+    connect(mSlider, &QSlider::valueChanged, [=](int value) {
+        mSpinBox->setValue((double)value / sliderMultiplier);
+    });
+    connect(mSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), [=](double value) {
+        mSlider->setValue((int)(value * sliderMultiplier));
+    });
+    connect(mSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &SliderSpinBox::valueChanged);
 }
 
 void SliderSpinBox::setSuffix(const QString &suffix)
@@ -31,13 +36,13 @@ void SliderSpinBox::setSuffix(const QString &suffix)
 
 void SliderSpinBox::setMinimum(double minimum)
 {
-    mSlider->setMinimum((int)minimum);
+    mSlider->setMinimum((int)(minimum * sliderMultiplier));
     mSpinBox->setMinimum(minimum);
 }
 
 void SliderSpinBox::setMaximum(double maximum)
 {
-    mSlider->setMaximum((int)maximum);
+    mSlider->setMaximum((int)(maximum * sliderMultiplier));
     mSpinBox->setMaximum(maximum);
 }
 
