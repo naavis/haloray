07421ab36aa4d121d2479a7a3b9f330c14342e5a
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-07-01 22:57:14 +0300

    Set surface format to run HaloRay on Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QtGlobal>
 #include <QApplication>
+#include <QSurfaceFormat>
 #include "version.h"
 #include "gui/mainWindow.h"
 
@@ -32,6 +33,14 @@ int main(int argc, char *argv[])
 {
     qInstallMessageHandler(logHandler);
     QApplication app(argc, argv);
+
+    QSurfaceFormat format;
+    format.setVersion(4, 4);
+    format.setProfile(QSurfaceFormat::OpenGLContextProfile::CoreProfile);
+    format.setSwapBehavior(QSurfaceFormat::SwapBehavior::DoubleBuffer);
+    format.setSwapInterval(1);
+    QSurfaceFormat::setDefaultFormat(format);
+
     MainWindow mainWindow;
     mainWindow.show();
 
