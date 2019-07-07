f9be24cd85729be4680f876d6cd3aabeaf1952f5
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-07-07 16:46:24 +0300

    Add Appveyor config file

---
+++ scripts/build.ps1
@@ -0,0 +1,19 @@
+# Windows build script for Appveyor
+$ErrorActionPreference = "Stop";
+
+mkdir build
+cd build
+cmake .. -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Release
+cd .\src\Release\
+& "${env:Qt5_DIR}\bin\qtenv2.bat"
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+& "${env:Qt5_DIR}\bin\windeployqt.exe" `
+    --no-quick-import `
+    --no-translations `
+    --no-webkit2 `
+    --no-angle `
+    --no-opengl-sw `
+    -no-system-d3d-compiler `
+    --release `
+    haloray.exe
