ad8e2420002a5f7e1d6b7000227e174cc7d670ce
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-07-15 00:01:36 +0300

    Make build fail if tests fail

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -9,8 +9,9 @@ cd build
 ctest --no-compress-output -T Test
 $testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
 $testReport
-$wc = New-Object 'System.Net.WebClient'
-$wc
-$res = $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
+$res = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
 $res
+if ($res.FailedCount -gt 0) {
+    throw "$($res.FailedCount) tests failed."
+}
 popd
