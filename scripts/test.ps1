474f206acab776163aedeb93b84c87f0a4476210
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-07-14 23:45:27 +0300

    Add debug line to testing PS script

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,10 +2,15 @@
 
 $ErrorActionPreference = "Stop";
 
+$env:APPVEYOR_JOB_ID
+
 pushd
 cd build
 ctest --no-compress-output -T Test
 $testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
-$wc = New-Object System.Net.WebClient
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
+$testReport
+$wc = New-Object 'System.Net.WebClient'
+$wc
+$res = $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
+$res
 popd
