8700ec0bdcb34139380545ecd1080da941f2edf1
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-07-15 12:28:21 -0700

    Remove diagnostic messages

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -10,17 +10,9 @@ ctest --no-compress-output -T Test
 $testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
 $testReport
 
-Write-host "test file path:"
-Write-host (Resolve-Path $testReport.FullName)
-
 $XSLInputElement = New-Object System.Xml.Xsl.XslCompiledTransform
 $XSLInputElement.Load("https://raw.githubusercontent.com/rpavlik/jenkins-ctest-plugin/master/ctest-to-junit.xsl")
-
-Write-host "transforming..."
 $XSLInputElement.Transform($(Resolve-Path $testReport.FullName), (Join-Path (Resolve-Path .) "ctest-to-junit-results.xml"))
-
-type ctest-to-junit-results.xml
-
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ctest-to-junit-results.xml))
 
