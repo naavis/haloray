e21e70ddd53bb273d2ce3db5a98c6e919337b9e6
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-07-15 11:45:42 -0700

    Upload result as artifact and use Resolve-Path

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -9,7 +9,8 @@ cd build
 ctest --no-compress-output -T Test
 $testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
 $testReport
-$res = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
+Push-AppveyorArtifact $testReport
+$res = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testReport.FullName))
 $res
 if ($res.FailedCount -gt 0) {
     throw "$($res.FailedCount) tests failed."
