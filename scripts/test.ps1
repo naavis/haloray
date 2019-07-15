7aaec114338be2939e50331b7ac1e59e24aae5d6
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-07-16 00:06:16 +0300

    Fetch local copy of ctest-to-junit.xsl file

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,23 +2,20 @@
 
 $ErrorActionPreference = "Stop";
 
-$env:APPVEYOR_JOB_ID
+$XSLInputElement = New-Object System.Xml.Xsl.XslCompiledTransform
+$XSLInputElement.Load($(Join-Path $env:APPVEYOR_BUILD_FOLDER "scripts\ctest-to-junit.xsl"))
 
 pushd
 cd build
 ctest --no-compress-output -T Test
-$testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
-$testReport
+foreach ($testReport in Get-ChildItem -Path .\Testing\*\Test.xml)
+{
+    $XSLInputElement.Transform($(Resolve-Path $testReport.FullName), (Join-Path (Resolve-Path .) "ctest-to-junit-results.xml"))
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ctest-to-junit-results.xml))
 
-$XSLInputElement = New-Object System.Xml.Xsl.XslCompiledTransform
-$XSLInputElement.Load("https://raw.githubusercontent.com/rpavlik/jenkins-ctest-plugin/master/ctest-to-junit.xsl")
-$XSLInputElement.Transform($(Resolve-Path $testReport.FullName), (Join-Path (Resolve-Path .) "ctest-to-junit-results.xml"))
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ctest-to-junit-results.xml))
-
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path $testReport.FullName))
-
-if ($res.FailedCount -gt 0) {
-    throw "$($res.FailedCount) tests failed."
+    if ($res.FailedCount -gt 0) {
+        throw "$($res.FailedCount) tests failed."
+    }
 }
 popd
