3f653e2e227c82eeb40e77bf62cbed52ac942acc
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-07-15 13:51:17 -0700

    Convert CTest results to JUnit format for AppVeyor
    CTest results don't seem to be xUnit compatible. They are
    pretty easy to convert to JUnit format with instructions from
    AppVeyor, though:
    https://help.appveyor.com/discussions/suggestions/5298-support-ctest-xml-test-results

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -9,8 +9,15 @@ cd build
 ctest --no-compress-output -T Test
 $testReport = Get-ChildItem -Path .\Testing\*\Test.xml | Select-Object -First 1
 $testReport
-$res = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
-$res
+
+$XSLInputElement = New-Object System.Xml.Xsl.XslCompiledTransform
+$XSLInputElement.Load("https://raw.githubusercontent.com/rpavlik/jenkins-ctest-plugin/master/ctest-to-junit.xsl")
+$XSLInputElement.Transform($(Resolve-Path $testReport.FullName), (Join-Path (Resolve-Path .) "ctest-to-junit-results.xml"))
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ctest-to-junit-results.xml))
+
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path $testReport.FullName))
+
 if ($res.FailedCount -gt 0) {
     throw "$($res.FailedCount) tests failed."
 }
