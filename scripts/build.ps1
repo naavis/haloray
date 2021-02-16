naavis/haloray

commit 3c06f4fb041b9d4d4007bf3ebce2e6bc2064ed3b

Update package and build scripts to work with Qt project files

scripts/build.ps1
@@ -1,24 +1,30 @@
 # Windows build script for Appveyor
 
-$gitBranch = $env:APPVEYOR_REPO_BRANCH
-$extraParameters = if ($gitBranch -eq "master") {
-    "-DHALORAY_VERSION='${env:APPVEYOR_BUILD_VERSION}'"
-} else {
-    "-DGIT_BRANCH='${gitBranch}'"
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+cmd /c "vcvars64.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
 }
+popd
+Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
 
-ls $env:Qt5_DIR
+$extraParameters = if ($gitBranch -eq "master") {
+    $version = ${env:APPVEYOR_BUILD_VERSION}.Split('-')[0]
+    "-DHALORAY_VERSION='${version}'"
+}
 
 pushd
 mkdir build
-cd build
+cd src
+& "${env:Qt5_DIR}\bin\qmake.exe" main.pro -o ..\build\
+cd ..\build
+nmake 2>$null
+popd
 
 pushd
-cmake .. -G "Visual Studio 15 2017 Win64" $extraParameters
-cmake --build . --config Release
-cd .\src\Release\
-& "${env:Qt5_DIR}\bin\qtenv2.bat"
-& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+cd build\haloray\release
 & "${env:Qt5_DIR}\bin\windeployqt.exe" `
     --no-quick-import `
     --no-translations `
@@ -28,19 +34,4 @@ cd .\src\Release\
     --no-system-d3d-compiler `
     --release `
     haloray.exe
-popd
-
-pushd
-cd .\tests\Release\
-& "${env:Qt5_DIR}\bin\windeployqt.exe" `
-    --no-quick-import `
-    --no-translations `
-    --no-webkit2 `
-    --no-angle `
-    --no-opengl-sw `
-    --no-system-d3d-compiler `
-    --release `
-    .
-popd
-
 popd

src/haloray-core/haloray-core.pro
@@ -7,6 +7,22 @@ win32:CONFIG += windows
 
 DEFINES += QT_DEPRECATED_WARNINGS
 
+GIT_COMMIT_HASH=$$system(git log -1 --format=%h)
+
+DEFINES += "GIT_COMMIT_HASH=\"$$GIT_COMMIT_HASH\""
+GIT_BRANCH = $$(APPVEYOR_REPO_BRANCH)
+isEmpty(GIT_BRANCH) {
+    LOCAL_GIT_BRANCH=$$system(git rev-parse --abbrev-ref HEAD)
+    DEFINES += "GIT_BRANCH=\"$$LOCAL_GIT_BRANCH\""
+} else {
+    DEFINES += "GIT_BRANCH=\"$$GIT_BRANCH\""
+}
+
+HALORAY_VERSION = $$(HALORAY_VERSION)
+!isEmpty(HALORAY_VERSION) {
+    DEFINES += "HALORAY_VERSION=\"$(HALORAY_VERSION)\""
+}
+
 HEADERS += \
     gui/addCrystalPopulationButton.h \
     gui/crystalModel.h \

.gitignore
@@ -1,4 +1,5 @@
 build/*
+build-*/
 .vscode/*
 src/version.h
 Testing/

CMakeLists.txt
@@ -1,48 +1,0 @@
-cmake_minimum_required(VERSION 3.1.0 FATAL_ERROR)
-project(HaloRay)
-include(Dart)
-
-# Optionally set the git branch in AppVeyor
-set(GIT_BRANCH "Local build" CACHE STRING "Git branch to show in dev builds")
-
-# Get the latest abbreviated commit hash of the working branch
-execute_process(
-    COMMAND git log -1 --format=%h
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_COMMIT_HASH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-add_definitions("-DGIT_COMMIT_HASH=${GIT_COMMIT_HASH}")
-add_definitions("-DGIT_BRANCH=${GIT_BRANCH}")
-
-if (HALORAY_VERSION)
-    # Strip running build number from version
-    string(REGEX REPLACE "-.+$" "" HALORAY_VERSION_NO_BUILD_NUMBER ${HALORAY_VERSION})
-    add_definitions(-DHALORAY_VERSION=${HALORAY_VERSION_NO_BUILD_NUMBER})
-endif()
-
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-enable_testing()
-
-if(MSVC)
-    add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-endif()
-
-if(CMAKE_VERSION VERSION_LESS "3.7.0")
-    # find includes in the corresponding build directories
-    set(CMAKE_INCLUDE_CURRENT_DIR ON)
-endif()
-
-# Instruct CMake to run moc automatically when needed
-set(CMAKE_AUTOMOC ON)
-# Instruct CMake to run Qt RCC for resource files
-set(CMAKE_AUTORCC ON)
-
-find_package(Qt5 COMPONENTS Core Widgets Gui Test REQUIRED)
-
-add_subdirectory(src)
-add_subdirectory(tests)

scripts/ctest-to-junit.xsl
@@ -1,36 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-	<xsl:output method="xml" indent="yes" />
-	<xsl:template match="/">
-		<testsuites>
-			<xsl:variable name="buildName" select="//Site/@BuildName"/>
-			<xsl:variable name="numberOfTests" select="count(//Site/Testing/Test)"/>
-			<xsl:variable name="numberOfFailures" select="count(//Site/Testing/Test[@Status!='passed'])" />
-			<testsuite name="CTest"
-				tests="{$numberOfTests}" time="0"
-				failures="{$numberOfFailures}"  errors="0"
-				skipped="0">
-			<xsl:for-each select="//Site/Testing/Test">
-					<xsl:variable name="testName" select="translate(Name, '-', '_')"/>
-					<xsl:variable name="duration" select="Results/NamedMeasurement[@name='Execution Time']/Value"/>
-					<xsl:variable name="status" select="@Status"/>
-					<xsl:variable name="output" select="Results/Measurement/Value"/>
-					<xsl:variable name="className" select="translate(Path, '/.', '.')"/>
-					<testcase classname="projectroot{$className}"
-						name="{$testName}"
-						time="{$duration}">
-						<xsl:if test="@Status!='passed'">
-							<failure>
-								<xsl:value-of select="$output" />
-							</failure>
-						</xsl:if>
-						<system-out>
-							<xsl:value-of select="$output" />
-						</system-out>
-					</testcase>
-				</xsl:for-each>
-			</testsuite>
-		</testsuites>
-	</xsl:template>
-</xsl:stylesheet>
-

scripts/package.ps1
@@ -9,7 +9,7 @@ else {
 }
 
 $destination = "${env:APPVEYOR_BUILD_FOLDER}\haloray-${version}.zip"
-$buildLocation = "${env:APPVEYOR_BUILD_FOLDER}\build\src\Release\*"
+$buildLocation = "${env:APPVEYOR_BUILD_FOLDER}\build\haloray\release\*"
 
-7z a $destination $buildLocation '-x!*.lib'
+7z a $destination $buildLocation '-x!*.lib' '-x!*.res' '-x!*.obj'
 7z a $destination "${env:APPVEYOR_BUILD_FOLDER}\*.md"

scripts/test.ps1
@@ -1,21 +1,22 @@
 # Windows test script for Appveyor
 
-$ErrorActionPreference = "Stop";
-
-$XSLInputElement = New-Object System.Xml.Xsl.XslCompiledTransform
-$XSLInputElement.Load($(Join-Path $env:APPVEYOR_BUILD_FOLDER "scripts\ctest-to-junit.xsl"))
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+cmd /c "vcvars64.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
+Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
 
 pushd
 cd build
-ctest --no-compress-output -T Test
-foreach ($testReport in Get-ChildItem -Path .\Testing\*\Test.xml)
-{
-    $XSLInputElement.Transform($(Resolve-Path $testReport.FullName), (Join-Path (Resolve-Path .) "ctest-to-junit-results.xml"))
-    $wc = New-Object 'System.Net.WebClient'
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ctest-to-junit-results.xml))
+nmake check TESTARGS="-o xunit-results.xml,xunitxml" 2>$null
 
-    if ($res.FailedCount -gt 0) {
-        throw "$($res.FailedCount) tests failed."
-    }
+foreach ($testReport in Get-ChildItem -Path xunit-results.xml -Recurse) {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", $testReport.FullName)
 }
+
 popd

commit ef1ee78f4173a62d1aa2b64ff241bacbbe3fc37d

Fix version numbering when doing a release from master branch
Adding a version number to the HaloRay window title bar was not
working correctly. It was broken in commit
3c06f4fb041b9d4d4007bf3ebce2e6bc2064ed3b when switching from CMake to
qmake.

scripts/build.ps1
@@ -9,9 +9,9 @@ cmd /c "vcvars64.bat&set" | ForEach-Object {
 Pop-Location
 Write-Host "`nVisual Studio 2019 Command Prompt variables set." -ForegroundColor Yellow
 
-$extraParameters = if ($gitBranch -eq "master") {
+if ($gitBranch -eq "master") {
   $version = ${env:APPVEYOR_BUILD_VERSION}.Split('-')[0]
-  "-DHALORAY_VERSION='${version}'"
+  $env:HALORAY_VERSION = "${version}"
 }
 
 Push-Location

src/haloray-core/haloray-core.pro
@@ -20,7 +20,7 @@ isEmpty(GIT_BRANCH) {
 
 HALORAY_VERSION = $$(HALORAY_VERSION)
 !isEmpty(HALORAY_VERSION) {
-    DEFINES += "HALORAY_VERSION=\"$(HALORAY_VERSION)\""
+    DEFINES += "HALORAY_VERSION=\"$$HALORAY_VERSION\""
 }
 
 HEADERS += \