3616f04e7445e16138e52238374c58d2fa09132c
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2022-08-01 16:39:53 +0300

    Make divergent halos brighter

--- src/haloray-core/resources/shaders/divergent.glsl
+++ src/haloray-core/resources/shaders/divergent.glsl
@@ -841,7 +841,7 @@ void main(void)
     float sunRadiance = daylightEstimate(wavelength);
 
     ivec2 pixelCoordinates = ivec2(resolution.x * normalizedCoordinates.x, resolution.y * normalizedCoordinates.y);
-    vec3 cieXYZ = totalWeight * sunRadiance * vec3(xFit_1931(wavelength), yFit_1931(wavelength), zFit_1931(wavelength));
+    vec3 cieXYZ = 10.0 * totalWeight * sunRadiance * vec3(xFit_1931(wavelength), yFit_1931(wavelength), zFit_1931(wavelength));
     mat3 xyzToSrgb = mat3(3.24096994, -0.96924364, 0.05563008, -1.53738318, 1.8759675, -0.20397696, -0.49861076, 0.04155506, 1.05697151);
     storePixel(pixelCoordinates, xyzToSrgb * cieXYZ);
 }
